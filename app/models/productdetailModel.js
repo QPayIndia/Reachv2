64aa5a87376f34e37801b853c53707195f580dec
QpayIndia <147687760+QPayIndia@users.noreply.github.com>
2024-07-22 15:14:19 +0530

    Rating Issue Fixed

--- app/models/productdetailModel.js
+++ app/models/productdetailModel.js
@@ -40,7 +40,15 @@ ProductDetail.getDetail = (model,result)=>{
             //                 getService(model.uid).then((services)=>{
             //                      getBrochure(model.uid).then((brochure)=>{
                                    getProductReview(model.productid).then((reviews)=>{
-                                        result(null,{status:"success",message:"Product Detail Fetched successfully ",data:data,review:reviews});
+                                    getProductReviewCount(model).then((userReview)=>{
+                                        let userRating = {"rating":0,review:""};
+                                        if(userReview.length > 0){
+                                            userRating = {"rating":userReview[0]['rating'],review:userReview[0]['review']}
+                                        }
+
+                                        result(null,{status:"success",message:"Product Detail Fetched successfully ",data:data,userRating: userRating,review:reviews});
+
+                                    })
     
                             })
             //                 })
@@ -62,7 +70,16 @@ ProductDetail.getDetail = (model,result)=>{
             //                 getService(model.uid).then((services)=>{
             //                      getBrochure(model.uid).then((brochure)=>{
                                   getServiceReview(model.productid).then((reviews)=>{
-                                        result(null,{status:"success",message:"Service Detail Fetched successfully ",data:data,review:reviews});
+
+                                    getServiceReviewCount(model.userid,model.productid).then((userReview)=>{
+                                        let userRating = {"rating":0,review:""};
+                                        if(userReview.length > 0){
+                                            userRating = {"rating":userReview[0]['rating'],review:userReview[0]['review']}
+                                        }
+
+                                        result(null,{status:"success",message:"Service Detail Fetched successfully ",data:data,userRating: userRating,review:reviews});
+
+                                    })
     
                             })
             //                 })
@@ -91,11 +108,21 @@ ProductDetail.addRating = ([productid,userid,rating,review,type],result)=>{
             rating:rating,
             review:review
         });
-        addProductRating(model).then((id)=>{
-            result(null,{status:"success",message:"Rating  Inserted Successfully",data:id});
-        }).catch((err)=>{
-            result(null,{status:"failure",message:err});
-        });
+        getProductReviewCount(model).then((data)=>{
+            if(data.length>0){
+                updateProductRating(model).then((id)=>{
+                    result(null,{status:"success",message:"Rating  Inserted Successfully",data:id});
+                }).catch((err)=>{
+                    result(null,{status:"failure",message:err});
+                });
+            }else{
+                addProductRating(model).then((id)=>{
+                    result(null,{status:"success",message:"Rating  Inserted Successfully",data:id});
+                }).catch((err)=>{
+                    result(null,{status:"failure",message:err});
+                });
+            }
+        })
     }else{
         const model = new ServiceRatingModel({
             serviceid : productid,
@@ -105,11 +132,23 @@ ProductDetail.addRating = ([productid,userid,rating,review,type],result)=>{
             review:review
         });
 
-        addServiceRating(model).then((id)=>{
-            result(null,{status:"success",message:"Rating  Inserted Successfully",data:id});
-        }).catch((err)=>{
-            result(null,{status:"failure",message:err});
-        });
+        getServiceReviewCount(model.userid,model.serviceid).then((data)=>{
+            if(data.length>0){
+                updateServiceRating(model).then((id)=>{
+                    result(null,{status:"success",message:"Rating  Inserted Successfully",data:id});
+                }).catch((err)=>{
+                    result(null,{status:"failure",message:err});
+                });
+            }else{
+                addServiceRating(model).then((id)=>{
+                    result(null,{status:"success",message:"Rating  Inserted Successfully",data:id});
+                }).catch((err)=>{
+                    result(null,{status:"failure",message:err});
+                });
+            }
+        })
+
+       
     }
 
     
@@ -131,8 +170,10 @@ function getProductDetail(model){
             //     res[i]['rating'] = 0;
             //     res[i]['review'] = 0;
             // }
-            if(res[0]['productimg'] != null){
-                res[0]['productimg'] ="http://ec2-3-108-62-163.ap-south-1.compute.amazonaws.com:8080"+ res[0]['productimg'];
+            if(res.length>0){
+                if(res[0]['productimg'] != null){
+                    res[0]['productimg'] ="http://ec2-3-108-62-163.ap-south-1.compute.amazonaws.com:8080"+ res[0]['productimg'];
+                }
             }
             
             resolve(res);
@@ -154,8 +195,10 @@ function getServiceDetail(model){
             //     res[i]['rating'] = 0;
             //     res[i]['review'] = 0;
             // }
-            if(res[0]['serviceimg'] != null){
-                res[0]['serviceimg'] ="http://ec2-3-108-62-163.ap-south-1.compute.amazonaws.com:8080"+ res[0]['serviceimg'];
+            if(res.length>0){
+                if(res[0]['serviceimg'] != null){
+                    res[0]['serviceimg'] ="http://ec2-3-108-62-163.ap-south-1.compute.amazonaws.com:8080"+ res[0]['serviceimg'];
+                }
             }
             
             resolve(res);
@@ -191,10 +234,39 @@ function getServiceReview(uid){
         })
     })
 }
+function getServiceReviewCount(userid,serviceid){
+    return new Promise((resolve,reject)=>{
+        sql.query("SELECT * FROM service_rating_master as B WHERE B.userid = ? AND B.serviceid = ?",[userid,serviceid],(err,res)=>{
+            if(err){
+                
+                console.log('Review Fail due to '+err);
+                reject();
+                return;
+            }
+            console.log('Review Fetched successfully');
+            resolve(res);
+        })
+    })
+}
 
 function getProductReview(uid){
     return new Promise((resolve,reject)=>{
         sql.query("SELECT A.name,B.rating,B.review,B.createdon FROM user_master as A,product_rating_master as B WHERE B.userid = A.uid AND B.productid = ? LIMIT 20",[uid],(err,res)=>{
+            if(err){
+                
+                console.log('Review Fail due to '+err);
+                reject();
+                return;
+            }
+            console.log('Review Fetched successfully');
+            resolve(res);
+        })
+    })
+}
+
+function getProductReviewCount(model){
+    return new Promise((resolve,reject)=>{
+        sql.query("SELECT * FROM product_rating_master as B WHERE B.userid = ? AND B.productid = ?",[model.userid,model.productid],(err,res)=>{
             if(err){
                 
                 console.log('Review Fail due to '+err);
@@ -295,7 +367,7 @@ function getBusinessHour(uid){
 }
 function addProductRating(model){
     return new Promise((resolve,reject)=>{
-        sql.query("INSERT INTO product_rating_master SET ?",[model],(err,res)=>{
+        sql.query("INSERT INTO product_rating_master SET ?;UPDATE product_master SET reviewCount = reviewCount + 1,totalRating = totalRating + ? WHERE productid = ?",[model,model.rating,model.productid],(err,res)=>{
             if(err){
                 
                 console.log('Rating Insert Fail due to '+err);
@@ -308,9 +380,26 @@ function addProductRating(model){
     })
 }
 
+function updateProductRating(model){
+    return new Promise((resolve,reject)=>{
+        getProductReviewCount(model).then((data)=>{
+            sql.query("UPDATE product_rating_master SET rating = ?,review = ? WHERE productid = ? AND userid = ?;UPDATE product_master SET totalRating = totalRating - ? + ? WHERE productid = ?;",[model.rating,model.review,model.productid,model.userid,data[0]['rating'],model.rating,model.productid],(err,res)=>{
+                if(err){
+                    
+                    console.log('Rating Update Fail due to '+err);
+                    reject();
+                    return;
+                }
+                console.log('Rating Updated successfully');
+                resolve();
+            })
+        })
+    })
+}
+
 function addServiceRating(model){
     return new Promise((resolve,reject)=>{
-        sql.query("INSERT INTO  service_rating_master SET ?",[model],(err,res)=>{
+        sql.query("INSERT INTO service_rating_master SET ?;UPDATE service_master SET reviewCount = reviewCount + 1,totalRating = totalRating + ? WHERE serviceid = ?",[model,model.rating,model.serviceid],(err,res)=>{
             if(err){
                 
                 console.log('Rating Insert Fail due to '+err);
@@ -319,6 +408,23 @@ function addServiceRating(model){
             }
             console.log('Rating Inserted successfully');
             resolve(res.insertId);
+        })
+    })
+}
+
+function updateServiceRating(model){
+    return new Promise((resolve,reject)=>{
+        getServiceReviewCount(model.userid,model.serviceid).then((data)=>{
+            sql.query("UPDATE service_rating_master SET rating = ?,review = ? WHERE serviceid = ? AND userid = ?;UPDATE service_master SET totalRating = totalRating - ? + ? WHERE serviceid = ?;",[model.rating,model.review,model.serviceid,model.userid,data[0]['rating'],model.rating,model.serviceid],(err,res)=>{
+                if(err){
+                    
+                    console.log('Rating Update Fail due to '+err);
+                    reject();
+                    return;
+                }
+                console.log('Rating Updated successfully');
+                resolve();
+            })
         })
     })
 }
