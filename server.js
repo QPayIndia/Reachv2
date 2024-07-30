cbda6ab71f29ec4ef717beb0d75bdeb9bf94c077
QpayIndia <147687760+QPayIndia@users.noreply.github.com>
2024-07-30 12:50:12 +0530

    Admin Module Created

--- server.js
+++ server.js
@@ -61,12 +61,12 @@ app.get('/uploads/kyc/:name', (req, res) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.sendFile("C:/Users/Public/Reach/reach/Reachv2/uploads/kyc/"+imagePath); 
 });
-app.get('/uploads/kyb/:name', (req, res) => { 
+app.get('/uploads/business/kyb/:name', (req, res) => { 
   const imagePath = req.params.name; 
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.sendFile("C:/Users/Public/Reach/reach/Reachv2/uploads/kyb/"+imagePath); 
+  res.sendFile("C:/Users/Public/Reach/reach/Reachv2/uploads/business/kyb/"+imagePath); 
 });
 
 require("./app/routes/businessInfoRoute.js")(app);
