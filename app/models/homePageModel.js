b0c6ca06dc5dead400d320dbbed5825d51b9f68c
QpayIndia <github@skandaapi.online>
2024-09-12 14:53:06 +0530

    commit

--- app/models/homePageModel.js
+++ app/models/homePageModel.js
@@ -39,7 +39,7 @@ HomePageModel.getManPower = (areaid,result)=>{
 
 function _getRentalProducts(){
     return new Promise((resolve,reject)=>{
-        sql.query("SELECT * FROM product_master WHERE rentFlag = 1",(err,data)=>{
+        sql.query("SELECT * FROM product_master WHERE rentFlag = 1 order by productid DESC",(err,data)=>{
             if(err){
                 resolve([]);
                 console.log("Get Product Data :"+err);
@@ -60,7 +60,7 @@ function _getRentalProducts(){
 
 function _getManPower(areaid){
     return new Promise((resolve,reject)=>{
-        sql.query("SELECT A.manreqid,A.bid,A.title,A.jobtype,A.description,A.skill,A.qualification,A.experience,A.agefrom,A.ageto,A.salaryfrom,A.salaryto,A.gender,A.employementtype,A.noofvacancies,A.startdate,A.enddate FROM manpower_req_master as A INNER JOIN location_master ON A.bid = location_master.uid AND location_master.areaid = ?;",[areaid],(err,data)=>{
+        sql.query("SELECT A.manreqid,A.bid,A.title,A.jobtype,A.description,A.skill,A.qualification,A.experience,A.agefrom,A.ageto,A.salaryfrom,A.salaryto,A.gender,A.employementtype,A.noofvacancies,A.startdate,A.enddate FROM manpower_req_master as A INNER JOIN location_master ON A.bid = location_master.uid AND location_master.areaid = ? order by A.manreqid DESC;",[areaid],(err,data)=>{
                 if(err){
                     reject();
                     console.log('Manpower Req Fetch Failed due to '+err);
