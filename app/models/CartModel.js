51a670b32c5a0296cdfe2a4ad6c22ab41681cfbd
QpayIndia <github@skandaapi.online>
2024-09-27 11:45:58 +0530

    Commit

--- app/models/CartModel.js
+++ app/models/CartModel.js
@@ -171,7 +171,8 @@ function CheckoutProductCart(userid){
                             return;
                         }
                         console.log('Checkout Items Added successfully');
-                            sql.query("INSERT INTO `transaction_master` (`userid`, `amount`, `transtype`, `orderid`, `paymentstatus`,`commissionpercentage`,`commissionamount`,`settlementamount`) SELECT userid,amount,'order',orderid,1,?,amount * "+global.pgCommission+"/100,amount - (amount * "+global.pgCommission+"/100) FROM order_master WHERE orderid = ?;",[orderid,global.pgCommission ],(err,res)=>{
+                        var query = "INSERT INTO `transaction_master` (`userid`, `amount`, `transtype`, `orderid`, `paymentstatus`,`commissionpercentage`,`commissionamount`,`settlementamount`) SELECT userid,amount,'order',orderid,1,?,amount * "+global.pgCommission+"/100,amount - (amount * "+global.pgCommission+"/100) FROM order_master WHERE orderid = ?;";
+                            sql.query(query,[orderid,global.pgCommission ],(err,res)=>{
                                 if(err){
                                     console.log('Transaction create Failed due to '+err);
                                     reject(err);
