11f85111996ecdb734b258e04a1977cc7e6635fb
QpayIndia <github@skandaapi.online>
2024-09-30 12:30:37 +0530

    commit

--- app/models/chatModel.js
+++ app/models/chatModel.js
@@ -284,7 +284,7 @@ function _getChats(roomid){
 
 function _getUnreadChats(roomid,chatid){
     return new Promise((resolve,reject)=>{
-        sql.query("SELECT * FROM chat_master WHERE roomid = ? AND chatid > ? ORDER BY chatid ASC;",[roomid,chatid],(err,data)=>{
+        sql.query("SELECT chatid,roomid,senderid,sendertype,message,media,mediatype,isseen,DATE_FORMAT(date, '%D %b %Y %l:%i %p') as date FROM chat_master WHERE roomid = ? AND chatid > ? ORDER BY chatid ASC;",[roomid,chatid],(err,data)=>{
                 if(err){
                     reject(err);
                     console.log('Chats Fetch Failed due to '+err);
