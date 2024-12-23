3c920e14a3a70b408bf98c73499e3f08b4010721
QpayIndia <mani@qpayindia.com>
2024-12-23 14:25:18 +0530

    Commit

--- app/models/TransactionModel.js
+++ app/models/TransactionModel.js
@@ -102,10 +102,22 @@ TransactionModel.UpdateTransactionResponse = (model,result)=>{
 }
 TransactionModel.getPaymentDetails = (transactionid,result)=>{
     
-
-    getPaymentDetails(transactionid).then((row)=>{
-        result(null,{status:"success",message:"Payment Details Fetched Successfully",data:row});
-    }).catch((err)=>{
+    _getTXNDetails(transactionid).then((data)=>{
+        if(data.transtype === 'bill'){
+            _getBillPaymentDetails(transactionid).then((row)=>{
+                result(null,{status:"success",message:"Payment Details Fetched Successfully",data:row});
+            }).catch((err)=>{
+                result(err,{status:"failure",message:"Payment Details Fetch Failed"});
+            })
+        }else{
+            getPaymentDetails(transactionid).then((row)=>{
+                result(null,{status:"success",message:"Payment Details Fetched Successfully",data:row});
+            }).catch((err)=>{
+                result(err,{status:"failure",message:"Payment Details Fetch Failed"});
+            })
+        }
+           
+        }).catch((err)=>{
         result(err,{status:"failure",message:"Payment Details Fetch Failed"});
     })
     
@@ -404,6 +416,37 @@ function getPaymentDetails(transactionid){
    
     return new Promise((resolve,reject)=>{
         sql.query("SELECT A.amount,DATE_FORMAT(A.createdon, '%h:%i %p , %d %M %Y') as date,paymentstatus as status ,B.name as businessname,D.phone as businessphone,C.name as username,C.phone as userphone  FROM `transaction_master`as A,`business_info` as B,`user_master` as C,`contact_info` as D WHERE A.bid = B.uid AND A.userid = C.uid AND A.bid = D.uid AND A.transactionid = ?;",[transactionid],(err,data)=>{
+            if(err){
+                console.log("Get Transaction Details Failed : "+err);
+                reject(err);
+                return;
+            }
+           console.log("Transaction Details Fetched Successfully");
+           
+
+           if(data.length> 0){
+            data[0]['status'] = payStatus[data[0]['status']]
+            if(data[0]['amount'] != null){
+                data[0]['amountinwords'] = numberToWords(data[0]['amount']);
+               }else{
+                data[0]['amountinwords'] = "Rs. "+data[0]['amount'];
+               }
+           }
+           
+           
+           resolve(data[0]);
+    
+           
+            
+        })
+    })
+}
+
+
+function _getBillPaymentDetails(transactionid){
+   
+    return new Promise((resolve,reject)=>{
+        sql.query("SELECT A.amount,DATE_FORMAT(A.createdon, '%h:%i %p , %d %M %Y') as date,paymentstatus as status ,B.billername as businessname,B.billid as businessphone,C.name as username,C.phone as userphone  FROM `transaction_master`as A,`bill_transaction_master` as B,`user_master` as C WHERE A.orderid = B.billid AND A.userid = C.uid  AND A.transactionid = ?;",[transactionid],(err,data)=>{
             if(err){
                 console.log("Get Transaction Details Failed : "+err);
                 reject(err);
